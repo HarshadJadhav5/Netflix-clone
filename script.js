c37074891aa59600355a5738d26ac6628c5a06c2
Harshad <harshadjadhav5114@gmail.com>
2024-08-16 22:49:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleDropdown(id) {
+    var content = document.getElementById(id);
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
